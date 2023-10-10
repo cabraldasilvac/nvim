0776008041181cc5d6693b745ac46af724c78223
Wanderley Cabral da Silva <wcabraldasilvac@gmail.com>
2023-10-10 18:20:07 -0300

    :rocket: refactor:update file

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,25 +1,53 @@
-local status_ok, configs = pcall(require, 'nvim-treesitter.configs,setup')
+-- Plugin: nvim-treesitter
+
+local status_ok, configs = pcall(require, 'nvim-treesitter.configs.setup')
 if not status_ok then
     return
   end
 
 configs {
-
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
- ensure_installed = { "c", "lua", "vim", "python3", "cpp", "javascript", "typescript", "query", "css", "java", "html" },
-  sync_install = false,
-    ignore_install = { "" },
-  --auto_install = true,
-  highlight = {
+ ensure_installed = { 
+     "c", "lua", "vim", "python3", "cpp", "javascript", "typescript", "java", 
+     "query", "sql", "php", "css", "html", "markdown", "markdown_inline" 
+ },
+
+sync_install = false,
+
+ignore_install = { "" },
+--auto_install = true,
+highlight = {
     enable = true,
     disble = {""},
     additional_vim_regex_highlighting = true,
+},
 
-  },
-
-  autopairs = {
+autopairs = {
     enable = true,
-  },
+},
+  
+rainbow = {
+    enable = true,
+--  disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+-- colors = {}, -- table of hex strings
+-- termcolors = {} -- table of colour name strings
+},
 
-  indent = { enable = true, disable = { "yaml"}},
+incremental_selection = {
+    enable = false,
+    keymaps = {
+        init_selection = '<CR>',
+        scope_incremental = '<CR>',
+        node_incremental = '<TAB>',
+        node_decremental = '<S-TAB>',
+    },
+},
+
+indent = { enable = true, disable = { "yaml"}},
+
+tree_docs = {
+    enable = true,
+}
 }
