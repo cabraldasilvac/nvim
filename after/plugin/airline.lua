a71c0ae93e4384305e1d3de1eb6ad5f134a7f385
Wanderley Cabral da Silva <wcabraldasilvac@gmail.com>
2023-10-10 18:18:06 -0300

    :rocket: feat:update setup airline.lua

--- after/plugin/airline.lua
+++ after/plugin/airline.lua
@@ -8,4 +8,4 @@ let g:airline#extensions#tabline#show_buffers = 1
 let g:airline#extensions#tabline#switch_buffers_and_tabs = 1
 let g:airline#extensions#tabline#tab_nr_type = 1
 
-let g.airline_theme='simple'
+--let g.airline_theme='simple'
