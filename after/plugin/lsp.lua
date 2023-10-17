71479ed7c070a389c2cafbf1a593d683c6ecc6ad
Wanderley Cabral da Silva <wcabraldasilvac@gmail.com>
2023-10-17 20:14:16 -0300

    :rocket: feat:update lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -29,7 +29,6 @@ require('mason-lspconfig').setup({
   handlers = {
     lsp.default_setup,
     lua_ls = function()
-        
 local lua_opts = lsp.nvim_lua_ls()
 require('lspconfig').lua_ls.setup(lua_opts)
     end,
