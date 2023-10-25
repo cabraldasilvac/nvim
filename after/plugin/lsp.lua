74ed1c01c8b5ac49514717c763862a8498ba64d6
Wanderley Cabral da Silva <wcabraldasilvac@gmail.com>
2023-10-25 15:11:20 -0300

    :rocket: feat:refactor lsp.lua plugins

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,36 +1,37 @@
-local lsp = require('lsp-zero')
+local lsp_zero = require('lsp-zero')
 
-lsp.preset("recommended")
+lsp_zero.preset("recommended")
 
-lsp.on_attach(function(client, bufnr)
-lsp.default_keymaps({buffer = bufnr})
---
+lsp_zero.on_attach(function(client, bufnr)
+    lsp_zero.default_keymaps({buffer = bufnr})
+    local opts = {buffer = bufnr}
+
+    vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', opts)
+
 end)
-
-lsp.set_preferences({
-    suggest_lsp_servers = false,
-    setup_servers_on_start = true,
-    set_lsp_keymaps = true,
-    configure_diagnostics = true,
-    cmp_capabilities = true,
-    manage_nvim_cmp = true,
-    call_servers = 'local',
-    sign_icons = {
-        error = '✘',
-        warn = '▲',
-        hint = '⚑',
-        info = '',
-      },
-    })
-
+-- Diagnostic icons
+lsp_zero.set_sign_icons({
+  error = '✘',
+  warn = '▲',
+  hint = '⚑',
+  info = '',
+})
+--
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'cssls','html','jsonls','tailwindcss','pyright','lua_ls','dockerls','tsserver','rust_analyzer','eslint','clangd'},
-  handlers = {
-    lsp.default_setup,
+ensure_installed = {'cssls','html','jsonls','tailwindcss','pyright','lua_ls','dockerls','tsserver','rust_analyzer','eslint','clangd'},
+handlers = {
+    lsp_zero.default_setup,
     lua_ls = function()
-local lua_opts = lsp.nvim_lua_ls()
+
+local lua_opts = lsp_zero.nvim_lua_ls()
 require('lspconfig').lua_ls.setup(lua_opts)
     end,
   },
 })
+-- requires
+require('lspconfig').tsserver.setup({})
+require('lspconfig').eslint.setup({})
+require('lspconfig').rust_analyzer.setup({})
